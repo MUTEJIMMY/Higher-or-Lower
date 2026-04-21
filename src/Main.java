b27e4ccf0bdae2caf8e51903735f8607271d37a4
MUTEJIMMY <abdirahmanabdullahi7@gmail.com>
2026-04-21 15:14:29 -0400

    starting out

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    Scanner scan  =  new Scanner(System.in);
+    Random rand = new Random();
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    int x = rand.nextInt(1,101);
+
+
+    System.out.println("I have a random number between 1 and 100. I want you to try to guess it in 7 guess");
+
+    int userGuess = scan.nextInt();
+
+
+
 }

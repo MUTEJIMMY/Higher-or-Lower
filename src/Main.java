f3c01d7137a2141743a760e39d6b687413d59509
MUTEJIMMY <abdirahmanabdullahi7@gmail.com>
2026-04-21 19:38:09 -0400

    Adding in input checkers

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -10,39 +11,74 @@ void main() {
 
     System.out.print("I have a random number between 1 and 100. I want you to try to guess it in 7 guesses: ");
 
-    int sevenOrLess = 0;
-    boolean untilGuessedRight = true;
 
-    while(untilGuessedRight && sevenOrLess < 7) {
+    int points = 0;
+    boolean repeat = true;
+    while(repeat) {
+        int sevenOrLess = 0;
+        boolean untilGuessedRight = true;
+        int userGuess = 0;
+        while (untilGuessedRight && sevenOrLess < 7) {
 
-    String hOrl = "";
+            String hOrl;
+            boolean c1 = true;
+            while (c1) {
+                try {
+                    userGuess = scan.nextInt();
+                    if (userGuess < 101 && userGuess > 0) {
+                        c1 = false;
+                    } else {
+                        System.out.print("Number Out of Bounds Enter a number between 1-100:");
+                    }
+                } catch (InputMismatchException e) {
+                    scan.nextLine();
+                    System.out.print("Wrong Type of Input, Please input a number between 1-100: ");
+                }
+            }
+            if ((userGuess < randomNum) && sevenOrLess < 6) {
 
-    int userGuess = scan.nextInt();
+                hOrl = "Higher";
 
-    if ((userGuess < randomNum) && sevenOrLess < 6) {
+                System.out.println(hOrl + " guess again: ");
 
-        hOrl = "Higher";
+            } else if ((userGuess > randomNum) && sevenOrLess < 6) {
 
-        System.out.println(hOrl+ " guess again: ");
+                hOrl = "Lower";
 
-    } else if ((userGuess > randomNum) && sevenOrLess < 6) {
+                System.out.println(hOrl + " guess again: ");
 
-        hOrl = "Lower";
+            } else if (userGuess == randomNum) {
+                System.out.println("You got it right");
 
-        System.out.println(hOrl+ " guess again: ");
+                untilGuessedRight = false;
+            } else {
 
-    } else if(userGuess == randomNum) {
-        System.out.println("You got it right");
+                System.out.println("You ran out of guesses idiot bum boy");
 
-        untilGuessedRight = false;
-    }else{
+            }
 
-        System.out.println("You ran out of guesses idiot bum boy");
+            sevenOrLess++;
+        }
+        points++;
 
+        System.out.print("Do you want to try to guess another number(Type Y for repeat, N to not repeat: ");
+
+        String reDo = "";
+
+        boolean c2 = true;
+        while(c2) {
+            try {
+                reDo = scan.nextLine();
+            } catch (Exception e) {
+
+            }
+            if ((reDo.equalsIgnoreCase("y")) || reDo.equalsIgnoreCase("n")){
+                c2 = false;
+            }
+            else{
+                System.out.print("Please type Y to repeat or N to not repeat: ");
+            }
+        }
     }
-
-    sevenOrLess++;
-}
-
 
 }

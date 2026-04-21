566df6b0ab65b9220c8ee7fa1b62e1ca78bacc1b
MUTEJIMMY <abdirahmanabdullahi7@gmail.com>
2026-04-21 15:29:17 -0400

    adding base game

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
 import java.util.*;
-import java.math.*;
-import java.io.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     Scanner scan  =  new Scanner(System.in);
     Random rand = new Random();
 
-    int x = rand.nextInt(1,101);
+    int randomNum = rand.nextInt(1,101);
+
+    System.out.print("I have a random number between 1 and 100. I want you to try to guess it in 7 guesses: ");
 
 
-    System.out.println("I have a random number between 1 and 100. I want you to try to guess it in 7 guess");
-
+boolean untilGuessedRight = true;
+while(untilGuessedRight) {
     int userGuess = scan.nextInt();
 
+    if (userGuess < randomNum) {
+        System.out.println("Lower");
+    } else if (userGuess > randomNum) {
+        System.out.println("Higher");
+    } else {
+        System.out.println("You got it right");
+        untilGuessedRight = false;
+    }
+}
 
 
 }
